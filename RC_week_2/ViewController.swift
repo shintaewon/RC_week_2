3024a321b29c212da06deb76ee0cffb46e3a41b8
Taewon Shin <tlsxodnjs147@gmail.com>
2022-08-13 20:24:23 +0900

    Initial Commit

---
+++ RC_week_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RC_week_2
+//
+//  Created by 신태원 on 2022/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
